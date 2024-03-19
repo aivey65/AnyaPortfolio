f6890125517ec979513da93f36c9afaee14737c4
Asha <39443120+aivey65@users.noreply.github.com>
2024-03-18 22:30:07 -0500

    Begin adding Tab component to break down Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Tab from './Tab';
 
 export default function Header({ mobile, setPageFunction, activeTab }) {
     return <div id="header">
         <nav>
             <ol>
-                <li className="nav_li" onClick={ () => setPageFunction(0) }>HOME</li>
-                <li className="nav_li" onClick={ () => setPageFunction(1) }>ABOUT</li>
-                <li className="nav_li" onClick={ () => setPageFunction(2) }>PROJECT</li>
-                <li className="nav_li" onClick={ () => setPageFunction(3) }>RESUME</li>
-                <li className="nav_li" onClick={ () => setPageFunction(4) }>CONTACT</li>
+                {<Tab/>}
+                <li className={activeTab == 0 ? "nav_li active" : "nav_li"} onClick={ () => setPageFunction(0) }>HOME</li>
+                <li className={activeTab == 1 ? "nav_li active" : "nav_li"} onClick={ () => setPageFunction(1) }>ABOUT</li>
+                <li className={activeTab == 2 ? "nav_li active" : "nav_li"} onClick={ () => setPageFunction(2) }>PROJECT</li>
+                <li className={activeTab == 3 ? "nav_li active" : "nav_li"} onClick={ () => setPageFunction(3) }>RESUME</li>
+                <li className={activeTab == 4 ? "nav_li active" : "nav_li"} onClick={ () => setPageFunction(4) }>CONTACT</li>
             </ol>
         </nav>
     </div>
---
+++ frontend/src/components/Tab.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Tab() {
+    return <li className="nav_li">
+
+    </li>
+}
